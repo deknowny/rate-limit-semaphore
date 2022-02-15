ad72cbb0880bf2dab1b44643c051239f8a7a6600
Yan Kurbatov <deknowny@gmail.com>
2022-02-15 21:01:54 +0300

    Add examples of two implementations

---
+++ examples/new_previous.py
@@ -0,0 +1,24 @@
+import asyncio
+import datetime
+import time
+
+from ralisem import FixedNewPreviousDelaySemaphore, TimeRateLimitSemaphoreBase
+
+
+async def task(task_no: int, sem: TimeRateLimitSemaphoreBase):
+    async with sem:
+        print(f"Task {task_no} continued at {time.monotonic():.3}s")
+
+
+async def main():
+    sem = FixedNewPreviousDelaySemaphore(
+        access_times=3, per=datetime.timedelta(seconds=1)
+    )
+    tasks = [
+        task(i, sem)
+        for i in range(1000)
+    ]
+    await asyncio.gather(*tasks)
+
+
+asyncio.run(main())
